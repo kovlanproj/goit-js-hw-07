fe89dc72a6290ff15c102cead568b215fdfd058e
kovlanproj <kozorizva@gmail.com>
2022-07-26 13:49:55 +0300

    fixed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -15,10 +15,13 @@ function onGalleryClick(e) {
   }
 
   const link = e.target.dataset.source;
-  globalInstance = basicLightbox.create(`<img width="1400" height="900" src="${link}">`);
+  globalInstance = basicLightbox.create(`<img width="1400" height="900" src="${link}">`, {
+    onShow: onModalShow,
+    onClose: onModalClose,
+  });
   globalInstance.show();
 
-  window.addEventListener('keydown', onEscKeyPress);
+  // window.addEventListener('keydown', onEscKeyPress);
 }
 
 function makeImageMarkup({ preview, original, description }) {
@@ -43,12 +46,19 @@ function addMarkup(placeRefForMarkup, galleryItems) {
   placeRefForMarkup.insertAdjacentHTML('beforeend', makeGalleryMarkup(galleryItems));
 }
 
+function onModalShow() {
+  window.addEventListener('keydown', onEscKeyPress);
+}
+
+function onModalClose() {
+  window.removeEventListener('keydown', onEscKeyPress);
+}
+
 function onEscKeyPress(event) {
   const ESC_KEY_CODE = 'Escape';
   const isEscKey = event.code === ESC_KEY_CODE;
 
   if (isEscKey) {
     globalInstance.close();
-    window.removeEventListener('keydown', onEscKeyPress);
   }
 }
